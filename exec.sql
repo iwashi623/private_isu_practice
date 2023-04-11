489e2ea005039ffc71641b168492609c7dc370c6
iwashi623 <wonderland.7878@gmail.com>
2023-04-12 02:05:04 +0900

    add index

--- exec.sql
+++ exec.sql
@@ -3,3 +3,5 @@ ALTER TABLE comments ADD INDEX index_comments_on_post_id_created_at(post_id, cre
 ALTER TABLE posts ADD INDEX index_posts_on_created_at(created_at);
 
 ALTER TABLE comments ADD INDEX index_comments_on_user_id(user_id);
+
+ALTER TABLE users ADD INDEX index_users_on_account_name(account_name);
